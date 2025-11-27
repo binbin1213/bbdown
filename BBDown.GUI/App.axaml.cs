78e218eaf5116b0dbcae6da106c4075bf7fe4cfb
binbin1213 <piaozhitian@gmail.com>
2025-11-27 11:57:18 +0800

    fix: Use XAML to define macOS menu to override default

--- BBDown.GUI/App.axaml.cs
+++ BBDown.GUI/App.axaml.cs
@@ -1,13 +1,8 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-
-using Avalonia.Controls;
 using BBDown.GUI.Views;
-using System.Runtime.InteropServices;
-using ReactiveUI;
-
-using Avalonia.Input;
+using System;
 
 namespace BBDown.GUI;
 
@@ -23,32 +18,21 @@ public partial class App : Application
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             desktop.MainWindow = new MainWindow();
-
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                var mainMenu = new NativeMenu();
-
-                var appMenuItem = new NativeMenuItem("BBDown GUI");
-                var appMenu = new NativeMenu();
-
-                var aboutItem = new NativeMenuItem("关于 BBDown GUI");
-                aboutItem.Command = ReactiveCommand.Create(() => { new AboutWindow().Show(); });
-                appMenu.Add(aboutItem);
-
-                appMenu.Add(new NativeMenuItemSeparator());
-
-                var quitItem = new NativeMenuItem("退出 BBDown GUI");
-                quitItem.Gesture = new KeyGesture(Key.Q, KeyModifiers.Meta);
-                quitItem.Command = ReactiveCommand.Create(() => { desktop.Shutdown(); });
-                appMenu.Add(quitItem);
-
-                appMenuItem.Menu = appMenu;
-                mainMenu.Add(appMenuItem);
-
-                NativeMenu.SetMenu(this, mainMenu);
-            }
         }
 
         base.OnFrameworkInitializationCompleted();
+    }
+
+    private void About_OnClick(object? sender, EventArgs e)
+    {
+        new AboutWindow().Show();
+    }
+
+    private void Quit_OnClick(object? sender, EventArgs e)
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.Shutdown();
+        }
     }
 }
